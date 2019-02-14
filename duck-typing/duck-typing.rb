fcd5a80b7ae6d6c9a3dc73be3c62de45b63d5c52
Ian Johnson <tacoda@pm.me>
2019-02-13 20:34:39 -0500

    Complete duck typing chapter

--- duck-typing/duck-typing.rb
+++ duck-typing/duck-typing.rb
@@ -1,1 +1,19 @@
 # Reducing Costs with Duck Typing
+
+# Treat objects as if they are defined by their behavior rather than by their class
+
+# Recognizing Hidden Ducks
+
+# - Case statements that switch on class
+# - kind_of? and is_a?
+# - responds_to?
+
+# Summary
+
+# Messages are at the center of object-oriented applications and they pass among
+# objects along public interfaces. Duck typing detaches these public interfaces
+# from specific classes, creating virtual types that are defined by what they do
+# instead of by who they are.
+# Duck typing reveals underlying abstractions that might otherwise be invisible.
+# Depending on these abstractions reduces risk and increases flexibility, making
+# your application cheaper to maintain and easier to change.
